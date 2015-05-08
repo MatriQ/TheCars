7568bea60e96f3b3359bdd03fbdc452323273d12
Matrixdom <matrixdom@126.com>
2015-05-08 20:15:51 +0800

    add game logic

--- src/app.js
+++ src/app.js
@@ -4,12 +4,18 @@
  *  **********************************/
 var Car = cc.Sprite.extend({
     type:0,//0 black 1 red
-    state:0,//0:停止 1:加速 2:最大速度 3:刹车减速
+    state:0,//0:停止 1:加速 2:最大速度 3:刹车减速 4:自然减速
     maxSpeed:10,
     accSpeedUp:1,//加速度s
     accAutoSpeedDown:0.1,//自然减速度
     accSpeedDown:3,//刹车减速度
+    autoSpeed:10,//默认速度
     speed:0,
+    carAngel:0,//车身方向，12点方向为0
+    steeredAngle:0,//方向盘旋转角度，12点方向为0
+    wheelbase:230,//轴距
+    autoTurn:false,//自动转向
+    autoRun:false,//自动开
     ctor:function(type,_maxspeed){
         this.type=type;
         if(type==0){
@@ -27,11 +33,14 @@ var Car = cc.Sprite.extend({
 
         this.attr({
             anchorX:0.5,
-            anchorY:0
+            anchorY:0.3
         });
         this.scheduleUpdate();
         //速度控制
         this.schedule(function(){
+            if(this.state==0){
+                return;
+            }
             if(this.state==1){
                 if(this.speed>=this.maxSpeed){
                     this.speed=this.maxSpeed;
@@ -50,20 +59,57 @@ var Car = cc.Sprite.extend({
                     this.speed-=this.accSpeedDown;
                 }
             }
+            else if(this.state==4){
+                if(this.speed<=0){
+                    this.speed==0;
+                    this.state=4;
+                }
+                else{
+                    this.speed=this.autoSpeed;
+                    //this.speed-=this.accAutoSpeedDown;
+                }
+            }
         },0.1);
     },
     update:function(dt) {
         if(this.speed>=0){
+            //更新位置
             this.y+=this.speed;
+            //更新车身角度
+            if(this.steeredAngle!=0){
+                //内半径=轴距/tan(前轮转角)+后轮中点宽度
+                var radius1=this.wheelbase/Math.tan( this.steeredAngle*Math.PI/180);
+                //行动半径
+                var radius2=radius1+this.getContentSize().width/2;
+
+                //转角=弧长/弧度=弧长*180/(PI*半径)
+                var ang=this.speed*180/(Math.PI*radius2);
+                this.x+=((radius1-Math.cos(ang*Math.PI/180)*radius2)*(this.steeredAngle>0?-1:1))/500*this.speed;
+                if(this.x<130+45)
+                    this.x=130+45;
+                if(this.x>640-(120+45))
+                    this.x=640-(120+45);
+                //this.rotation=ang*30;
+            }
+
         }
     },
     Refuel:function(){
         //加油
         this.state=1;
     },
+    SpeedDown:function(){
+        //自然减速
+        this.state=4;
+    },
     Brake:function(){
         //刹车
         this.state=3;
+    },
+    Stop:function(){
+        //直接停止
+        this.state=0;
+        this.speed=0;
     },
     //是否在运行
     isRun:function(){
@@ -80,9 +126,9 @@ var Road=cc.Layer.extend({
     spBaks:{},
     spLines:null,
     spPlants:null,
+    roadSide:{l:130,r:640-130},//道路边界
     ctor:function() {
         this._super();
-
         //背景
         var spBg1 =this.spBaks.bg1= new cc.Sprite(res.img_BG);
         spBg1.anchorX=0;
@@ -100,21 +146,22 @@ var Road=cc.Layer.extend({
         this.spLines=new Array(8);
         for (var i = 0; i < 8; i++) {
             var spSpiteLine=this.spLines[i] = new cc.Sprite(res.img_roadLine);
-            spSpiteLine.x=i % 2==0?230:(640-230);
+            spSpiteLine.x=i % 2==0?260:(640-260);
             spSpiteLine.y=0 + 300 * (Math.floor(i / 2) + 1);
             this.addChild(spSpiteLine);
         }
 
         //植物
-        var count=Math.ceil(960/250)+1;
+        var count=2;
         this.spPlants=new Array(count*2);
         for(var i=0;i<count;i++){
             //左边
             var plant=new cc.Sprite(res.img_grass);
-            plant.y=-10+i*250;
+            plant.y=960*i;
             plant.anchorY=0;
             plant.anchorX=0;
-            this.addChild(plant,30,30+i*2);
+            plant.flippedY=i%2!=0;
+            this.addChild(plant);
             this.spPlants[i*2]=plant;
             //右边
             var plant=new cc.Sprite(res.img_grass);
@@ -122,8 +169,9 @@ var Road=cc.Layer.extend({
             plant.anchorY=0;
             plant.flippedX=true;
             plant.x=640;
-            plant.y=-10+i*250;
-            this.addChild(plant,30,30+i*2+1);
+            plant.y=960*i;
+            plant.flippedY=i%2!=0;
+            this.addChild(plant);
             this.spPlants[i*2+1]=plant;
         }
 
@@ -132,9 +180,12 @@ var Road=cc.Layer.extend({
     },
     onEnter:function(){
         this._super();
-        this.runAction(cc.follow(this.carBlack));
+        //this.runAction(cc.follow(this.carBlack));
+        this.schedule(this.updateBlock,4);
     },
     update:function(dt){
+        //follow
+        this.y=400-this.carBlack.y;
         //背景
         if(this.spBaks.bg2.y+this.y<=960/2){
             this.spBaks.bg1.y=this.spBaks.bg2.y+960;
@@ -162,11 +213,32 @@ var Road=cc.Layer.extend({
         var count=this.spPlants.length/2;
         for(var i=0;i<count;i++){
             var spPlant=this.spPlants[i*2];
-            if(spPlant.y+this.y+260<=0){
-                spPlant.y+=250*count;
+            if(spPlant.y+this.y<=-960){
+                spPlant.y+=960*2;
                 this.spPlants[i*2+1].y=spPlant.y;
             }
         }
+
+        //车辆碰撞检测
+        if(cc.rectIntersectsRect( this.carBlack.getBoundingBox(),this.carRed.getBoundingBox())){
+            if(this.carRed.y>this.carBlack.y){
+                this.carRed.Brake();
+                this.carBlack.Stop();
+                if(this.carRed.state==0){
+                    //如果停止状态 加入被撞位移
+                    this.carRed.runAction(cc.moveBy(0.2,cc.p(0,10)));
+                }
+            }
+            else{
+                if(this.carBlack.state==0){
+                    //如果停止状态 加入被撞位移
+                    this.carBlack.runAction(cc.moveBy(0.2,cc.p(0,10)));
+                }
+                this.carRed.Stop();
+                this.carBlack.Brake();
+            }
+        }
+
     },
     setCars:function(car1,car2){
         this.carRed=car1;
@@ -177,6 +249,46 @@ var Road=cc.Layer.extend({
     getCarDistance:function(){
         if(this.carRed!=null && this.carBlack!=null){
             return Math.abs(this.carRed.getPositionY()-this.carBlack.getPositionY());
+        }
+    },
+    updateBlock:function(){
+        //自动阻挡
+        //阻挡玩家
+        if(this.carRed.state!=0 && this.carBlack.state!=0){
+            //黑车在后面 红车随机转向
+            if(this.carRed.y-this.carBlack.y>100){
+                if(this.carRed.steeredAngle!=40){
+                    this.carRed.steeredAngle=40;
+                }
+                else{
+                    this.carRed.steeredAngle=-40;
+                }
+                /*if(this.carRed.x-this.carBlack.x>30){
+                    this.carRed.steeredAngle=-20;
+                }
+                else if(this.carRed.x-this.carBlack.x<-30){
+                    this.carRed.steeredAngle=20;
+                }
+                else{
+                    this.carRed.steeredAngle=0;
+                }*/
+            }
+            //黑车在前面
+            if(this.carBlack.y-this.carRed.y>100){
+                var dist=Math.sqrt(Math.pow((this.carBlack.x-this.carRed.x),2)+Math.pow((this.carBlack.y-this.carRed.y),2))
+                if(dist>300){
+                    if(this.carRed.x-this.carBlack.x>30){
+                        this.carRed.steeredAngle=30;
+                    }
+                    else if(this.carRed.x-this.carBlack.x<-30){
+                        //if(this.carRed.x)
+                        this.carRed.steeredAngle=-30;
+                    }
+                    else{
+                        this.carRed.steeredAngle=0;
+                    }
+                }
+            }
         }
     }
 });
@@ -193,7 +305,7 @@ var MainScene = cc.Scene.extend({
         this.road = new Road();
 
         var carRed=new Car(1);
-        var carBlack=new Car(0,13);
+        var carBlack=new Car(0,11);
         this.road.setCars(carRed,carBlack);
 
         this.addChild(this.road);
@@ -208,31 +320,42 @@ var MainScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
 
+        this.road.carRed.autoRun=true;
         this.road.carRed.Refuel();
         this.scheduleUpdate();
 
-        if ('mouse' in cc.sys.capabilities) {
+        if ('touch' in cc.sys.capabilities) {
             cc.eventManager.addListener({
-                event: cc.EventListener.MOUSE,
-                onMouseDown: function (event) {
+                event: cc.EventListener.TOUCH,
+                onTouchBegin: function (touch, event) {
                     var target = event.getCurrentTarget();
                     target.labDistence.color= cc.color._getGreen();
                     //加油门
                     target.road.carBlack.Refuel();
 
-                    target.touchStartPosi=event.getLocation();
+                    target.touchStartPosi=touch.getLocation();
+                    return true;
                 },
-                onMouseMove: function (event) {
+                onTouchMoved: function (touch,event) {
                     var target = event.getCurrentTarget();
                     if(target.road.carBlack.isRun() && target.touchStartPosi!=null){
-                        var newPosi=event.getLocation();
-                        var offsetX=newPosi.y-target.touchStartPosi.y;
-                        target.road.carBlack.x+=offsetX*0.1;
+                        var newPosi=touch.getLocation();
+                        var offsetX=newPosi.x-target.touchStartPosi.x;
+                        //target.road.carBlack.steeredAngle= Math.max(-20,Math.min(30,offsetX*3));
+                        if(offsetX>=5){
+                            target.road.carBlack.steeredAngle=5;
+                        }
+                        else if(offsetX<=-5){
+                            target.road.carBlack.steeredAngle=-5;
+                        }
+                        else{
+                            target.road.carBlack.steeredAngle=0;
+                        }
                     }
                 },
-                onMouseUp: function (event) {
+                onTouchEnded: function (touch,event) {
                     var target = event.getCurrentTarget();
-                    target.road.carBlack.Brake();
+                    target.road.carBlack.SpeedDown();
                     target.touchStartPosi=null;
                     target.labDistence.color = cc.color._getRed();
                 }
